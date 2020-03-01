cd2f8109389a978739f21441a0fee3e775b0a373
sulemaez <steve.oyeyo@gmail.com>
2020-03-01 18:59:46 +0300

    add models

---
+++ src/main/java/com/soyeyo/movies/controllers/WebController.java
@@ -0,0 +1,15 @@
+package com.soyeyo.movies.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WebController {
+
+   @RequestMapping(value = "/",method = RequestMethod.GET)
+   public String getIndex(){
+       return "index";
+   }
+}
---
+++ src/main/java/com/soyeyo/movies/models/Category.java
@@ -0,0 +1,4 @@
+package com.soyeyo.movies.models;
+
+public class Category {
+}
